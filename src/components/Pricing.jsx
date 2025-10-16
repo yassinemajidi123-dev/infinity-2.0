890734bb151de2988c894245d630d5e2babdb012
yassinemajidi123-dev <yassine.majidi123@gmail.com>
2025-10-16 12:26:34 +0100

    Update Pricing.jsx

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -5,7 +5,7 @@ export default function Pricing() {
     {
       title: "3 Months",
       badge: "For 1 Screen",
-      price: "£25,00",
+      price: "£25.00",
       highlight: false,
       features: [
         "SD/HD/Full HD/4K quality",
@@ -41,7 +41,7 @@ export default function Pricing() {
     {
       title: "1 Year",
       badge: "For 1 Screen",
-      price: "£48,00",
+      price: "£48.00",
       highlight: true, // highlighted like your right-most blue card
       features: [
         "SD/HD/Full HD/4K Quality",
